14fa44cb1c5e181cb9582447543d29675d53eadb
FCasadei <federico.casadei@marcegaglia.com>
2026-05-18 15:13:10 +0200

    Threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use log::{info,error,warn,debug,trace,LevelFilter};
 use env_logger;
+use std::{thread, time::Duration};
 
 fn just_fortytwo()->u8{
 	42
@@ -10,8 +11,13 @@ struct ABC{
 	b:String,
 }
 
-fn return_abc()->ABC{
-	ABC { a: 500, b: "Ehilà".to_string() }
+fn return_abc(in_value:u8)->Result<ABC,String>{
+	if in_value%5==0 || in_value%2==0{
+		Ok(ABC{ a: 500, b: "Ehilà".to_string() })
+	}
+	else{
+		Err("Aiuto".to_string())
+	}
 }
 
 fn main() {
@@ -19,10 +25,20 @@ fn main() {
 		.filter_level(LevelFilter::Trace)
 		.init();
 
+	let handle= thread::spawn( || {
+		for i in 1..10{
+			match return_abc(i){
+				Ok(appoggino)=>debug!("{}:{}",appoggino.a,appoggino.b),
+				Err(erroraccio)=>error!("{erroraccio}")
+			}
+			thread::sleep(Duration::from_millis(500));
+		}
+	});
+
 	info!("Hello, world!");
-	let appoggio:ABC = return_abc();
-	debug!("{}:{}",appoggio.a,appoggio.b);
 	warn!("I don't feel very good world: my fever is {}°C!",just_fortytwo());
 	error!("goodbye world");
 	trace!("ghooooost");
+
+	handle.join().unwrap();
 }
